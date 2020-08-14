294d3b3d4bc72e09c98f1e03357e7c63e187a147
james <james@DESKTOP-JDJA6NK>
2020-08-14 13:32:01 +0100

    - Added: Basic UI and ViewModels

---
+++ src/LifeOS.Common/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LifeOS.Common.Sys.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace LifeOS.Common.ViewModels
+{
+
+  public class BaseViewModel : INotifyPropertyChangedEx
+  {
+
+    [JsonIgnore]
+    public bool IsChanged { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+  }
+
+}
--- src/LifeOS.WPF/Views/Windows/MainWindow.xaml.cs
+++ src/LifeOS.WPF/Views/Windows/MainWindow.xaml.cs
@@ -32,17 +32,102 @@
 
 namespace LifeOS.WPF.Views.Windows
 {
+  using System.Collections.Generic;
+  using System.Collections.ObjectModel;
+  using System.Linq;
   using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Media;
+  using LifeOS.Common.Models;
+  using LifeOS.Common.ViewModels;
   using MahApps.Metro.Controls;
+  using NodaTime;
+
+  public class MainViewModel : BaseViewModel
+  {
+    
+    public DayPlanViewModel DayPlan { get; set; }
+    
+    public MainViewModel()
+    {
+
+      DayPlan = new DayPlanViewModel(new DayPlan());
+
+    }
+
+  }
 
   /// <summary>Interaction logic for MainWindow.xaml</summary>
   public partial class MainWindow : MetroWindow
   {
     #region Constructors
 
+    public DayPlanViewModel Plan { get; set; }
+    public ObservableCollection<DayActivityViewModel> ActivityTemplates { get; set; }
+    
     public MainWindow()
     {
+      
       InitializeComponent();
+
+
+      var timeslot = new ActivityTimeSlot();
+      timeslot.Duration = NodaTime.Duration.FromHours(1);
+      timeslot.Time = new OffsetTime(
+        new LocalTime(15, 20, 48),
+        Offset.FromHours(0));
+
+      var subtimeslot = new ActivityTimeSlot();
+      subtimeslot.Duration = NodaTime.Duration.FromHours(0.5);
+      subtimeslot.Time = new OffsetTime(
+        new LocalTime(15, 20, 48),
+        Offset.FromHours(0));
+
+      var subactivity = new DayActivity(subtimeslot, "Incremental Reading", Enumerable.Empty<DayActivity>());
+      var activity1 = new DayActivity(timeslot, "SuperMemo", new List<DayActivity> { subactivity });
+
+
+      var plan = new DayPlan();
+      plan.Activities = new ObservableCollection<DayActivity>();
+      plan.Activities.Add(activity1);
+
+      Plan = new DayPlanViewModel(plan);
+
+      ActivityTemplates = new ObservableCollection<DayActivityViewModel>
+      {
+
+        new DayActivityViewModel(
+          new DayActivity(
+            new ActivityTimeSlot(NodaTime.Duration.FromHours(1), new OffsetTime(new LocalTime(15, 20, 48), Offset.FromHours(0))),
+            "Hello World",
+            Enumerable.Empty<DayActivity>()))
+
+      };
+
+      this.DataContext = this;
+
+    }
+
+    private void Expander_Expanded(object sender, RoutedEventArgs e)
+    {
+      for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
+        if (vis is DataGridRow)
+        {
+          var row = (DataGridRow)vis;
+          row.DetailsVisibility = row.DetailsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+          break;
+        }
+    }
+
+    private void Expander_Collapsed(object sender, RoutedEventArgs e)
+    {
+      for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
+        if (vis is DataGridRow)
+        {
+          var row = (DataGridRow)vis;
+          row.DetailsVisibility = row.DetailsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+          break;
+        }
     }
 
     #endregion
